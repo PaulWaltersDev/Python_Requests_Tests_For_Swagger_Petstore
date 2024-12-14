34970693af483da2236e1f8d338ee7dffb8b132a
PaulWaltersDev <paulwalters2002@yahoo.co.uk>
2024-12-14 22:19:17 +1100

    First upload of pet search tests, upload image and data files

---
+++ comparison_data/comparison_pets/comparison_pets.py
@@ -0,0 +1,18 @@
+pet_single = {
+  "id": 9223372016900014281,
+  "category": {
+    "id": 0,
+    "name": "string"
+  },
+  "name": "Puff",
+  "photoUrls": [
+    "string"
+  ],
+  "tags": [
+    {
+      "id": 0,
+      "name": "string"
+    }
+  ],
+  "status": "available"
+}
